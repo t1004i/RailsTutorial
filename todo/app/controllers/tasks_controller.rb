8236126b6b9288c11690a6f1093f9895b2c281cd
t inoue <tinoue.insta201806@gmail.com>
2018-08-28 11:48:53 +0900

    add todo dir

---
+++ todo/app/controllers/tasks_controller.rb
@@ -0,0 +1,37 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+  def new
+    @task = Task.new
+  end
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+  def edit
+    @task = Task.find(params[:id])
+  end
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to root_path
+    else
+      render edit_path
+    end
+  end
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to root_path
+  end
+
+  private
+    def task_params
+      params[:task].permit(:title)
+    end
+end
