0a45229bb693a63f68df1199dfdd8aa4c23627c7
t1004i <dazkikensinshi@gmail.com>
2018-09-09 14:27:30 +0000

    add contents

--- microblog/app/controllers/posts_controller.rb
+++ microblog/app/controllers/posts_controller.rb
@@ -1,22 +1,49 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all.order(updated_at: "desc")
   end
 
   def new
+    @post = Post.new
   end
-
+  
   def create
+      if Post.create(body_params)
+          flash[:notice] = "作成しました。"
+          redirect_to posts_path
+      else
+          render new_post_path
+      end
   end
-
+  
   def show
   end
-
+  
   def edit
+    @post = Post.find(params[:id])
+    
   end
-
+  
   def update
+    if Post.update(body_params)
+      flash[:notice] = "#{params[:id]}番目を更新しました。"
+      redirect_to posts_path
+    else
+      render edit_post_path(params[:id])
+    end
+    
   end
-
+  
   def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      flash[:notice] = "削除しました。"
+      redirect_to posts_path
+    end
+  end
+  
+  private
+  def body_params
+      params.require(:post).permit(:body)
   end
 end
--- microblog/config/routes.rb
+++ microblog/config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
-  get 'posts/index'
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/show'
-  get 'posts/edit'
-  get 'posts/update'
-  get 'posts/destroy'
+  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
